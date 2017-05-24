850051da7e68f8c663960cd7e3fcc837474bba3c
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-05-24 04:27:22 +0000

    removed glasses from pic

--- js/profile.js
+++ js/profile.js
@@ -2,6 +2,19 @@ var summary = new Vue({
   el: '#summary',
   data: {
     heading: 'Summary',
-    summary: 'This is a summary of me.'
+    summary: 'This is a summary of me.',
+    
   }
-})
+});
+var experience = new Vue({
+    el: '#experience',
+    data: {
+        heading: 'Experience',
+        jobs: [
+            {logo: '', dates: '2014-Present', company: 'Citrus City Technology', position: 'Web Developer'},
+            {logo: '', dates: '2015-2016', company: 'Spicy Monkey Media', position: 'Programmer'},
+            {logo: '', dates: '2011-2014', company: 'Planet Herbs, Inc.', position: 'Sales Support Technician'}
+            ]
+    }
+    
+});
