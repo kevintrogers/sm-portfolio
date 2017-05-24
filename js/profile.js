cf1eb69403c86d454ca90d525d48d295919d0fe3
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-05-24 04:32:30 +0000

    added details array to experience vue

--- js/profile.js
+++ js/profile.js
@@ -9,12 +9,37 @@ var summary = new Vue({
 var experience = new Vue({
     el: '#experience',
     data: {
-        heading: 'Experience',
+        heading: 'Work History',
         jobs: [
-            {logo: '', dates: '2014-Present', company: 'Citrus City Technology', position: 'Web Developer'},
-            {logo: '', dates: '2015-2016', company: 'Spicy Monkey Media', position: 'Programmer'},
-            {logo: '', dates: '2011-2014', company: 'Planet Herbs, Inc.', position: 'Sales Support Technician'}
+            {logo: '', dates: '2014-Present', company: 'Citrus City Technology', position: 'Web Developer',
+                details: [
+                    {
+                        
+                    }
+                    ]
+            },
+            {logo: '', dates: '2015-2016', company: 'Spicy Monkey Media', position: 'Programmer',
+                details: [
+                    {
+                        
+                    }
+                    ]
+            },
+            {logo: '', dates: '2011-2014', company: 'Planet Herbs, Inc.', position: 'Sales Support Technician',
+                details: [
+                    {
+                        
+                    }
+                    ]
+            }
             ]
+    }
+});
+var education = new Vue({
+    el: '#education',
+    data: {
+        heading: 'Education',
+        
     }
     
 });
