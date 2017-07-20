a806f33395fe2d39202e26a639678120de289b96
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-07-20 04:15:08 +0000

    finished home section and added fonts to test profile page

---
+++ js/profile-vue.js
@@ -0,0 +1,22 @@
+var skills = new Vue({
+  el: '#skill-list',
+  data: {
+    skills: [
+        { skill: 'WordPress'},
+        { skill: 'Meteor JS'},
+        { skill: 'Vue JS'}
+    ],
+  }
+});
+  var solutions = new Vue({
+  el: '#solution-list',
+  data: {
+    solutions: [
+        {solution: 'Full Stack Development'},
+        {solution: 'Web Design'},
+        {solution: 'Hosting'},
+        {solution: 'Asset Creation'},
+        {solution:'SEO'}
+        ]
+  }
+});
