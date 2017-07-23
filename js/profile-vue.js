146f517203d6956f867f34ae58450b338d05c7d5
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-07-23 13:01:18 +0000

    unified styles on all sections

--- js/profile-vue.js
+++ js/profile-vue.js
@@ -4,14 +4,14 @@
   el: '#projects',
   data: {
     projects: [
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http://example.com'},
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
-        {title: 'Coming Soon', img='https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'}
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http://example.com'},
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'},
+        {title: 'Coming Soon', img:'https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif', url: 'http:example.com'}
        ]
   }
 })
