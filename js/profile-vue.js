a211a4a86dfcc2d79693f025ccbdd381de11305e
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-08-13 05:02:01 +0000

    added social media links to vue file

--- js/profile-vue.js
+++ js/profile-vue.js
@@ -1,6 +1,39 @@
+var socialLinks = new Vue({
+  el: '#social-links',
+  data: {
+      social: [
+      
+        {
+            title:'Github',
+            icon: "<i class='fa fa-github-alt' aria-hidden='true'></i>",
+            link:'https://github.com/kevintrogers'
+        },
+        {
+            title:'Code Pen',
+            icon: "<i class='fa fa-codepen' aria-hidden='true'></i>",
+            link:'https://codepen.io/MonkeyMaker/'
+        },
+        {
+            title:'Facebook',
+            icon: "<i class='fa fa-facebook' aria-hidden='true'></i>",
+            link:'https://www.facebook.com/spicymonkeymedia'
+        },
+        {
+            title:'Twitter',
+            icon: "<i class='fa fa-twitter' aria-hidden='true'></i>",
+            link:'https://twitter.com/spicymonkymedia'
+        },
+        {
+            title:'Linkedin',
+            icon: "<i class='fa fa-linkedin' aria-hidden='true'></i>",
+            link:'https://www.linkedin.com/profile/view?id=AAIAAAmBUKoBdbOrqAwzQRxcyyy6wp_9xl90TVk&trk=nav_responsive_tab_profile'
+        }
+          
+      ]
+  }
+});
 
-
-  var projects = new Vue({
+var projects = new Vue({
   el: '#projects',
   data: {
     projects: [ 
